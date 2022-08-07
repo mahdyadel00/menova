a1fdc5de86deebec66ba14356f25fe7f4a3fa309
mahdyadel00 <mahdyadel00@gmail.com>
2022-08-07 21:49:51 +0200

    logo

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="container d-flex justify-content-between">
     <div class="logo">
-     <a href="/"><img src="{{URL::asset('frontend/assets/img/logo.jpeg')}}" alt="logo" class="img-fluid"></a>
+     <a href="/"><img src="{{URL::asset('frontend/assets/img/logo.png')}}" alt="logo" class="img-fluid" style="width:340px"></a>
     </div>
   <!-- navbar -->
   @include('frontend.layouts.navbar')
