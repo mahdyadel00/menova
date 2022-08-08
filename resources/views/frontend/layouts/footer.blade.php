98ca8ff928707046ee58f7e9689faa0147a1b1f7
mahdyadel00 <mahdyadel00@gmail.com>
2022-08-08 21:41:53 +0200

    done footer

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -19,9 +19,11 @@
     <div class="container">
       <div class="row gy-4">
         <div class="col-lg-5 col-md-12 footer-info">
-          <a href="http://menovahub.com" class="logo d-flex align-items-center">
-            <img src="{{ asset('frontend') }}/assets/img/logo.jpeg" alt="">
-            <span>@lang('site.menovahub')</span>
+                <a href="/"><img src="{{URL::asset('frontend/assets/img/logo.png')}}" alt="logo" class="img-fluid" style="width:320px"></a>
+
+          {{-- <a href="http://menovahub.com" class="logo d-flex align-items-center"> --}}
+            {{-- <img src="{{ asset('frontend') }}/assets/img/logo.jpeg" alt="">
+            <span>@lang('site.menovahub')</span> --}}
           </a>
           <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
           <div class="social-links mt-3">
